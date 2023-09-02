92937814d2af58a943bada4427282fb6b4768ee4
Saquib Hussain <109733382+Hussainaquib@users.noreply.github.com>
2023-09-02 19:39:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 st.title("""
-Explore different Classifier on different Datasets
+Explore Different Datasets with Different Classifier
 """)
 
 # A side_bar in which we select the required Dataset
